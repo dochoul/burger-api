d89fab8b5c10a61cdefff91a00818323469e1bb1
dochoul <dochoul@gmail.com>
2023-11-03 16:51:31 +0900

    update

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const router = jsonServer.router("posts.json");
+// const router = jsonServer.router("db.json");
+// const router = jsonServer.router("posts.json");
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "index" });
+});
+
+router.get("/posts", function (req, res, next) {
+  res.render("index", { title: "posts" });
+});
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
